27e4527fae435ef9f48d4abba41ea93afca573cf
SyneArt <syneart@users.noreply.github.com>
2014-07-17 10:30:06 +0800

    support x64 now

--- bctim.bat
+++ bctim.bat
@@ -1,24 +1,23 @@
 :: Copyright 2014 by SyneArt <sa@syneart.com>
 @echo off&cls&title Install&color 0F&mode con cols=38 lines=10
 setlocal enableextensions enabledelayedexpansion
-echo.&echo.&echo.&echo.&echo Install ConnectivityLab Theme Now ..
-set str=%PATH: =/%&set MiKTeXPath=nul
-FOR %%b in ("%str%") DO (
+echo.&echo.&echo.&echo.&echo Install Theme Now ..
+set str=!PATH: =/!&set str=!str:(x86)=[x86]!&set MiKTeXPath=nul
+FOR %%b in (!str!) DO (
 	set nowStr=%%b&set MiKTeXStr=x!nowStr:MiKTeX=!
 	if not !MiKTeXStr!==x!nowStr! set MiKTeXPath=!nowStr!
 )
-if %MiKTeXPath%==nul (
+if !MiKTeXPath!==nul (
 	cls&echo.&echo.&echo.&echo.&echo 請先安裝 MiKTeX, 再執行本安裝
 	color E3&set /p=&goto exit
 )
-set MiKTeXPath=%MiKTeXPath:miktex\bin\=%
-set MiKTeXPath=%MiKTeXPath:/= %
-if not exist "%MiKTeXPath%tex\latex\beamer\" (
+set MiKTeXPath=!MiKTeXPath:miktex\bin\=!&set MiKTeXPath=!MiKTeXPath:/= !&set MiKTeXPath=!MiKTeXPath:[x86]=(x86)!
+if not exist "!MiKTeXPath!tex\latex\beamer\" (
 	cls&echo.&echo.&echo.&echo.&echo 請先安裝 Beamer, 再執行本安裝
 	color E3&set /p=&goto exit
 )
 call:copyFile .\themes, *.sty&call:copyFile .\art, *.jpg
-initexmf --update-fndb 1>nul 2>nul
+initexmf --update-fndb >nul
 if !errorlevel!==1 (
 	cls&echo.&echo.&echo.&echo.&echo 請先停止編譯 MiKTeX 專案, 再執行
 	color 4C&set /p=&goto exit
@@ -28,7 +27,7 @@ cls&echo.&echo.&echo.&echo.&echo Install Theme Successful！&color 2A&ping 127.0
 :copyFile 
 FOR /R %~1 %%c in (%~2) DO (
 	set themesFile=%%c&set themesFile=!themesFile:%~dp0=!
-	copy "%%c" "%MiKTeXPath%tex\latex\beamer\base\!themesFile!" 1>nul 2>nul
+	copy "%%c" "!MiKTeXPath!tex\latex\beamer\base\!themesFile!" >nul
 )
 
 :exit
