b8816a2065fa9cb0fc6e6b0f46d923ab24978e53
SyneArt <syneart@users.noreply.github.com>
2014-07-19 15:03:51 +0800

    Update bctim.bat

--- bctim.bat
+++ bctim.bat
@@ -1,25 +1,25 @@
 :: Copyright 2014 by SyneArt <sa@syneart.com>
 @echo off&cls&title Install&color 0F&mode con cols=38 lines=10
 setlocal enableextensions enabledelayedexpansion
-echo.&echo.&echo.&echo.&echo Install ConnectivityLab Theme Now ..
+echo.&echo.&echo.&echo.&echo Install Theme Now ..
 set str=!PATH: =/!&set str=!str:(x86)=[x86]!&set MiKTeXPath=nul
 FOR %%b in (!str!) DO (
 	set nowStr=%%b&set MiKTeXStr=x!nowStr:MiKTeX=!
 	if not !MiKTeXStr!==x!nowStr! set MiKTeXPath=!nowStr!
 )
 if !MiKTeXPath!==nul (
-	cls&echo.&echo.&echo.&echo.&echo ½Ð¥ý¦w¸Ë MiKTeX, ¦A°õ¦æ¥»¦w¸Ë
+	cls&echo.&echo.&echo.&echo.&echo è«‹å…ˆå®‰è£ MiKTeX, å†åŸ·è¡Œæœ¬å®‰è£
 	color E3&set /p=&goto exit
 )
 set MiKTeXPath=!MiKTeXPath:miktex\bin\=!&set MiKTeXPath=!MiKTeXPath:/= !&set MiKTeXPath=!MiKTeXPath:[x86]=(x86)!
 if not exist "!MiKTeXPath!tex\latex\beamer\" (
-	cls&echo.&echo.&echo.&echo.&echo ½Ð¥ý¦w¸Ë Beamer, ¦A°õ¦æ¥»¦w¸Ë
+	cls&echo.&echo.&echo.&echo.&echo è«‹å…ˆå®‰è£ Beamer, å†åŸ·è¡Œæœ¬å®‰è£
 	color E3&set /p=&goto exit
 )
 call:copyFile .\themes, *.sty&call:copyFile .\art, *.jpg
 initexmf --update-fndb >nul
 if !errorlevel!==1 (
-	cls&echo.&echo.&echo.&echo.&echo ½Ð¥ý°±¤î½sÄ¶ MiKTeX ±M®×, ¦A°õ¦æ
+	cls&echo.&echo.&echo.&echo.&echo è«‹å…ˆåœæ­¢ç·¨è­¯ MiKTeX å°ˆæ¡ˆ, å†åŸ·è¡Œ
 	color 4C&set /p=&goto exit
 )
 cls&echo.&echo.&echo.&echo.&echo Install Theme Successful ^^!&color 2A&ping 127.0.0.1 -n 3 >nul&goto exit
