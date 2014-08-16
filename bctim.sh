c3dd8a4986c9bc6cf231a6f4beabbb4110cf184a
SyneArt <syneart@users.noreply.github.com>
2014-08-16 12:58:12 +0800

    support Mac finish

--- bctim.sh
+++ bctim.sh
@@ -3,13 +3,13 @@
 copyFile () {
 	DIRPATHS=null
 	CPPATHS=null
-	for ori in `ls -R $(dirname "$0")/$1`
+	for lori in `ls -R /dev/null $(dirname "$0")/$1 2>/dev/null`
 	do
-		if [ "${ori##*:}" == "" ]; then
-			DIRPATHS=${ori//:/}
+		if [ "${lori##*:}" == "" ]; then
+			DIRPATHS=${lori//:/}
 		else
-			CPPATHS=${DIRPATHS}/$ori
-			sudo cp -f ${CPPATHS} ${installPATH}/beamer/base${DIRPATHS//$(dirname "$0")/} >/dev/null 2>&1
+			CPPATHS=$DIRPATHS/$lori
+			sudo cp -f $CPPATHS $installPATH/${DIRPATHS//$(dirname "$0")/} >/dev/null 2>&1
 		fi
 	done
 }
@@ -17,15 +17,19 @@ clear
 echo "Info: Install Theme Now .."
 OS=`uname -s`
 if [ "${OS}" == "Darwin" ] ; then
-	installPATH=`tlmgr --version | grep 'tlmgr using installation:' | cut -d " " -f 4`
+	installPATH=`tlmgr --version | grep 'tlmgr using installation:' | cut -d " " -f 4`/texmf-dist/tex/latex/beamer/
 elif [ "${OS}" == "Linux" ] ; then
 	if [ -f /etc/lsb-release ] ; then
-		installPATH=/usr/share/texmf/tex/latex
+		installPATH=/usr/share/texmf/tex/latex/beamer/base/
 	else
-		installPATH=“”
+		installPATH=/dev/null
 	fi
 fi
-if [ ! -d $installPATH/beamer ]; then
+if [[ $installPATH == /dev/null ]]; then
+	clear
+	echo "Error: 不支援此 Linux 版本, 請回報以獲得支援！"
+	exit 0
+elif [ ! -d $installPATH ]; then
 	clear
 	echo "Error: 請先安裝 Beamer, 再執行本安裝"
 	exit 0
@@ -40,8 +44,8 @@ if [ $? -eq 1 ]; then
 fi
 clear
 echo "Info: Install Theme Now .."
-copyFile "themes" ".sty"
-copyFile "art" ".jpg"
+copyFile themes .sty
+copyFile art .jpg
 sudo texhash >/dev/null 2>&1
 clear
 echo "Info: Install Theme Successful !"
