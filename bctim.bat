7914725eeed2f185c6fc183070c17952e2d344e9
SyneArt <syneart@users.noreply.github.com>
2016-01-22 11:23:31 +0800

    Fix ErrorLevel in function

--- bctim.bat
+++ bctim.bat
@@ -15,7 +15,7 @@ call tlmgr --version 1>nul 2>&1 && (
 	set texPath=!texPath!\texmf-dist\tex\latex\beamer\
 	call:copyFile .\themes, *.sty&call:copyFile .\art, *.jpg
 	texhash >nul 2>&1
-	call:checkError
+	if !errorlevel!==1 (call:checkError&goto exit)
 )
 
 REM For MiKTeX
@@ -29,7 +29,7 @@ call initexmf --version 1>nul 2>&1 && (
 	set texPath=!texPath!\tex\latex\beamer\base\
 	call:copyFile .\themes, *.sty&call:copyFile .\art, *.jpg
 	initexmf --admin --update-fndb >nul 2>&1
-	call:checkError
+	if !errorlevel!==1 (call:checkError&goto exit)
 )
 
 if !texPath!==nul (
@@ -37,18 +37,13 @@ if !texPath!==nul (
 	color E3&set /p=&goto exit
 )
 
-if !errorlevel!==0 (
-	cls&echo.&echo.&echo.&echo.&echo Install Theme Successful ^^! :-P&color 2A&ping 127.0.0.1 -n 3 >nul&goto exit
-)
-goto exit
+cls&echo.&echo.&echo.&echo.&echo Install Theme Successful ^^! :-P&color 2A&ping 127.0.0.1 -n 3 >nul&goto exit
 
 :checkError
-if !errorlevel!==1 (
-	cls&echo.&echo.&echo.&echo.&echo Please stop LaTeX Compile first.
-	color 4C&set /p=&goto exit
-)
+cls&echo.&echo.&echo.&echo.&echo Please stop LaTeX Compile first.
+color 4C&set /p=
 
-:copyFile 
+:copyFile
 FOR /R %~1 %%c in (%~2) DO (
 	set themesFile=%%c&set themesFile=!themesFile:%~dp0=!
 	copy /Y "%%c" "!texPath!!themesFile!" >nul
