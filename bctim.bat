866b83e6307dfbd26e58998d3c5a35863538aa3b
SyneArt <syneart@users.noreply.github.com>
2016-01-29 02:25:52 +0800

    Fix MiKTeX path error and show more feedback msg.

--- bctim.bat
+++ bctim.bat
@@ -14,22 +14,22 @@ call tlmgr --version 1>nul 2>&1 && (
 	)
 	set texPath=!texPath!\texmf-dist\tex\latex\beamer\
 	call:copyFile .\themes, *.sty&call:copyFile .\art, *.jpg
+	if !errorlevel!==1 (call:checkCopyError&goto exit)
 	texhash >nul 2>&1
-	if !errorlevel!==1 (call:checkError&goto exit)
+	if !errorlevel!==1 (call:checkCompileError&goto exit)
 )
 
 REM For MiKTeX
 call initexmf --version 1>nul 2>&1 && (
-	for /f "tokens=1* delims=:" %%a in ('initexmf --report') do (
-		if "%%a"=="UserInstall" (
-			set texPath="%%b"&set texPath=!texPath:" =!
-			set texPath=!texPath:"=!
-		)
+	for /f "tokens=1* delims=." %%a in ('findexe initexmf.exe') do (
+		echo %%a | FINDSTR "initexmf" 1>nul 2>&1 && set texPath=%%a
 	)
+	set texPath=!texPath:\miktex\bin\initexmf=!
 	set texPath=!texPath!\tex\latex\beamer\base\
 	call:copyFile .\themes, *.sty&call:copyFile .\art, *.jpg
+	if !errorlevel!==1 (call:checkCopyError&goto exit)
 	initexmf --admin --update-fndb >nul 2>&1
-	if !errorlevel!==1 (call:checkError&goto exit)
+	if !errorlevel!==1 (call:checkCompileError&goto exit)
 )
 
 if !texPath!==nul (
@@ -39,14 +39,18 @@ if !texPath!==nul (
 
 cls&echo.&echo.&echo.&echo.&echo Install Theme Successful ^^! :-P&color 2A&ping 127.0.0.1 -n 3 >nul&goto exit
 
-:checkError
+:checkCopyError
+cls&echo.&echo.&echo.&echo.&echo LaTeX Path Error, please feedback.
+color 4C&set /p=&goto exit
+
+:checkCompileError
 cls&echo.&echo.&echo.&echo.&echo Please stop LaTeX Compile first.
-color 4C&set /p=
+color 4C&set /p=&goto exit
 
 :copyFile
 FOR /R %~1 %%c in (%~2) DO (
 	set themesFile=%%c&set themesFile=!themesFile:%~dp0=!
-	copy /Y "%%c" "!texPath!!themesFile!" >nul
+	copy /Y "%%c" "!texPath!!themesFile!" 1>nul 2>&1
 )
 
 :exit
