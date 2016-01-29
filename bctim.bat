49f37ee41c661ec3bf73408fce789d07e8dc70d9
SyneArt <syneart@users.noreply.github.com>
2016-01-30 06:09:12 +0800

    Add Show Progress

--- bctim.bat
+++ bctim.bat
@@ -1,11 +1,10 @@
 :: Copyright by SyneArt <sa@syneart.com>
 @echo off&cls&title InstallTheme&color 0F&mode con cols=38 lines=10
 setlocal enableextensions enabledelayedexpansion
-echo.&echo.&echo.&echo.&echo Install Theme Now , Please Wait .. 
 set texPath=nul
 
 REM For TeXLive
-call tlmgr --version 1>nul 2>&1 && (
+call:pushProgress 1 & call tlmgr --version 1>nul 2>&1 && (
 	for /f "tokens=1* delims=:" %%a in ('tlmgr --version') do (
 		if "%%a"=="tlmgr using installation" (
 			set texPath=#%%b&set texPath=!texPath:# =!\texmf-dist\tex\latex\beamer\
@@ -13,19 +12,19 @@ call tlmgr --version 1>nul 2>&1 && (
 	)
 	call:copyFile .\themes, *.sty&call:copyFile .\art, *.jpg
 	if !errorlevel!==1 (call:pushCopyError&goto exit)
-	texhash >nul 2>&1
+	call:pushProgress 2 & texhash >nul 2>&1
 	if !errorlevel!==1 (call:pushCompileError&goto exit)
 )
 
 REM For MiKTeX
-call initexmf --version 1>nul 2>&1 && (
+call:pushProgress 3 & call initexmf --version 1>nul 2>&1 && (
 	for /f "tokens=1* delims=: " %%a in ('initexmf --report') do (
 		if exist %%b\tex\latex\beamer\base\ set texPath=%%b\tex\latex\beamer\base\
 	)
 	if !texPath!==nul (call:pushBeamerError&goto exit)
 	call:copyFile .\themes, *.sty&call:copyFile .\art, *.jpg
 	if !errorlevel!==1 (call:pushCopyError&goto exit)
-	initexmf --admin --update-fndb >nul 2>&1
+	call:pushProgress 4 & initexmf --admin --update-fndb >nul 2>&1
 	if !errorlevel!==1 (call:pushCompileError&goto exit)
 )
 
@@ -47,6 +46,10 @@ color 4C&set /p=&goto exit
 :pushCompileError
 cls&echo.&echo.&echo.&echo.&echo Please stop LaTeX Compile first.
 color 4C&set /p=&goto exit
+
+:pushProgress
+cls&echo.&echo.&echo.&echo.&echo Installing Theme , Wait .. [ %~1 of 4 ]
+goto exit
 
 :copyFile
 FOR /R %~1 %%c in (%~2) DO (
